0b2b2ba2b5d8fd0f0a122df05f3449cfea13462d
Alemontoya2022 <tatomontoya2014@gmail.com>
2023-09-05 23:48:36 -0300

    Parte de la clase 31/08, dividimos HTML para mas orden

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', curso),
     path('lista-cursos/', listar_cursos),
-    path('/', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entragables),
+    path('', inicio, name="Inicio"),
+    path('cursos/', cursos, name="Cursos"),
+    path('profesores/', profesores, name="Profesores"),
+    path('estudiantes/', estudiantes, name="Estudiantes"),
+    path('entregables/', entragables, name="Entregables"),
     ]
